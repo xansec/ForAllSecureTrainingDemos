29dc028bcbb526903da3ef8002774798d0c5c5b9
Alex Brewer <abrewer@forallsecure.com>
2021-09-01 15:33:13 -0400

    Rename stuff

--- fuzzing-fundamentals-lighttpd/runmayhem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mayhem login
-
-#vuln
-mayhem run -f Mayhemfile.vuln .
-
-#fixed
-mayhem run -f Mayhemfile.fixed .
--- fuzzing-fundamentals-lighttpd/runserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#vulnerable version
-docker run lighttpd:1.14.15
-#fixed version
-#docker run lighttpd:1.14.52
