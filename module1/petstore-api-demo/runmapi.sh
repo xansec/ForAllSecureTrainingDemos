616ff1b32555af3fa48dd9d81eca53fa4d2e348a
xansec <76011430+xansec@users.noreply.github.com>
2021-11-11 15:07:53 -0500

    Update runmapi.sh

--- module1/petstore-api-demo/runmapi.sh
+++ module1/petstore-api-demo/runmapi.sh
@@ -3,7 +3,7 @@
 mapi run \
      petstore-demo 120 'http://localhost/openapi.json' \
      --header-auth 'api_key: special-key' \
-     --header-auth 'Authorization: Bearer ab421254-1411-42ec-b587-85ad69c1b190' \
+     --header-auth 'Authorization: Bearer 123' \
      --sarif 'output.sarif' \
      --url 'http://localhost/v3/' \
      --interactive
