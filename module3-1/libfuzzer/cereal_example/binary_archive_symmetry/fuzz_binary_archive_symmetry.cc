f92d6dbf51f5037db07a39f01877001fc5907c94
Alex Brewer <abrewer@forallsecure.com>
2022-04-04 17:21:16 -0400

    update Cereal example

--- module3-1/libfuzzer/cereal_example/binary_archive_symmetry/fuzz_binary_archive_symmetry.cc
+++ module3-1/libfuzzer/cereal_example/binary_archive_symmetry/fuzz_binary_archive_symmetry.cc
@@ -3,6 +3,11 @@
 #include <cereal/types/string.hpp>
 #include <cereal/archives/binary.hpp>
 
+int neverCalled() {
+  return 1337;
+}
+
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
 	std::string o_string(data, data+size);
@@ -18,6 +23,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 		cereal::BinaryInputArchive iar(is);
 		iar(i_string);
 	}
+	
 	if (i_string != o_string) abort();
 
 	return 0;
