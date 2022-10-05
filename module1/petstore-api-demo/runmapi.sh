7ceaa1a91797a03c6c4c35a7864510c7556a9991
Alex Brewer <abrewer@forallsecure.com>
2022-10-05 18:07:57 -0400

    Mapi updates

--- module1/petstore-api-demo/runmapi.sh
+++ module1/petstore-api-demo/runmapi.sh
@@ -3,7 +3,7 @@
 set -ex
 
 mapi run \
-     forallsecure/petstore-demo 120 'http://localhost/openapi.json' \
+     forallsecure/petstore-demo 30 'http://localhost/openapi.json' \
      --header-auth 'api_key: special-key' \
      --sarif 'output.sarif' \
      --url 'http://localhost/v3/' \
