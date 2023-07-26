2675a1f3648928dd76247a2145050c12e92e8b8d
Alexander Brewer <abrewer@forallsecure.com>
2023-07-26 19:17:56 -0400

    New release 2.5 JIRA tools

--- tools/bts_export.py
+++ tools/bts_export.py
@@ -6,6 +6,7 @@ import csv
 import datetime as dt
 import json
 import logging
+import subprocess
 from enum import Enum, auto
 from requests.auth import HTTPBasicAuth
 
@@ -24,9 +25,10 @@ def testAPI(url, headers):
         sys.exit(1)
     return
 
-def getDefect(api, headers, defect_id):
+def getDefect(api, headers, workspace, project, target, defect_id):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
-    endpoint = api + '/defect/' + defect_id
+    base = api['mayhem']['url'] + '/api/v2/owner/' + workspace + '/project/' + project + '/target/' + target
+    endpoint = base + '/defect/' + defect_id
     try:
         response = session.request('GET', endpoint, headers=headers)
         result = response.json()
@@ -36,51 +38,60 @@ def getDefect(api, headers, defect_id):
         sys.exit(1)
     return [result]
 
-def getDefectsForRun(api, headers, run_id, offset=0):
+def getDefectsForRun(api, headers, workspace, project, target, run_id, offset=0):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
-    endpoint = api + '/run/' + run_id + '/defect?per_page=' + str(ELEMENTS) + '&offset=' + str(offset)
+    base = api['mayhem']['url'] + '/api/v2/owner/' + workspace + '/project/' + project + '/target/' + target
+    endpoint = base + '/run/' + run_id + '/defect?per_page=' + str(ELEMENTS) + '&offset=' + str(offset)
     try:
         response = session.request('GET', endpoint, headers=headers)
         results = response.json()
         if len(results['defects']) == ELEMENTS:
-            results.update(getDefectsForRun(api, headers, run_id, (offset + ELEMENTS)))
+            results['defects'].append(getDefectsForRun(api, headers, workspace, project, target, run_id, (offset + ELEMENTS)))
     except KeyError as e:
         logging.error('KeyError:' + str(e) + ', check your parameters.')
         sys.exit(1)
-    return result['defects']
+    return results['defects']
 
-def exportToJira(api, headers, issue_data):
+def exportToJira(api, headers, issue_data, dry_run):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
     endpoint = api['jira']['url'] + '/rest/api/2/issue/'
     auth = HTTPBasicAuth(api['jira']['username'], api['jira']['token'])
-    try:
-        response = session.request('POST', endpoint, headers=headers, json=issue_data, auth=auth)
-        resp_dict = json.loads(response.text)
-        print('Issue ' + str(resp_dict['key']) + ' created.')
-    except KeyError as e:
-        logging.error('Issue not created, check your permssions and parameters.')
-        logging.error(e)
-        logging.error(resp_dict)
-        sys.exit(1)
+    if dry_run:
+        logging.debug(issue_data)
+        return endpoint
+    else:
+        try:
+            response = session.request('POST', endpoint, headers=headers, json=issue_data, auth=auth)
+            resp_dict = json.loads(response.text)
+            print('Issue ' + str(resp_dict['key']) + ' created.')
+        except KeyError as e:
+            logging.error('Issue not created, check your permssions and parameters.')
+            logging.error(e)
+            logging.error(resp_dict)
+            sys.exit(1)
     return api['jira']['url'] + '/browse/' + str(resp_dict['key'])
 
-def exportToGitlab(api, headers, issue_data):
+def exportToGitlab(api, headers, issue_data, dry_run):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
     endpoint = api['gitlab']['url'] + '/api/v4/projects/' + str(api['gitlab']['project-id']) + '/issues/'
-    try:
-        response = session.request('POST', endpoint, headers=headers, json=issue_data)
-        resp_dict = json.loads(response.text)
-        print('Issue ' + str(resp_dict['iid']) + ' created.')
-    except KeyError as e:
-        logging.error('Issue not created, check your permssions and parameters.')
-        logging.error(e)
-        logging.error(resp_dict)
-        sys.exit(1)
+    if dry_run:
+        logging.debug(issue_data)
+        return endpoint
+    else:
+        try:
+            response = session.request('POST', endpoint, headers=headers, json=issue_data)
+            resp_dict = json.loads(response.text)
+            print('Issue ' + str(resp_dict['iid']) + ' created.')
+        except KeyError as e:
+            logging.error('Issue not created, check your permssions and parameters.')
+            logging.error(e)
+            logging.error(resp_dict)
+            sys.exit(1)
     return resp_dict['web_url']
 
 
 OFFSET = 0
-ELEMENTS = 100
+ELEMENTS = 20
 # --todo-- Add additional fields as needed here
 
 JIRA_FORMAT = '''
@@ -112,17 +123,18 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--namespace', required=True, type=str, help='The namespace for the project (usually either the username or org)')
+    parser.add_argument('--workspace', required=True, type=str, help='The workspace for the project')
     parser.add_argument('--project', required=True, type=str, help='The name of the project')
     parser.add_argument('--target', required=True, type=str, help='The name of the target')
     parser.add_argument('--bts', required=True, type=str, help='The type of BTS you want to export to (choices: \'jira\', \'gitlab\')')
     parser.add_argument('--defect', type=str, help='The defect number to export (exports a single defect)')
     parser.add_argument('--run', type=str, help='The run number to export (exports all defects in a run)')
-    parser.add_argument('--config', type=str, default='bts.config', help='The BTS configuration file (defaults to \'bts.config\')')
-    parser.add_argument('--url', type=str, help='The Mayhem for Code URL')
-    parser.add_argument('--token', type=str, help='The Mayhem for Code API access token')
+    parser.add_argument('--bts-config', type=str, default='bts.config', help='The BTS configuration file (defaults to \'bts.config\')')
+    parser.add_argument('--mayhem-config', type=str, default='mayhem.config', help='The Mayhem configuration file (defaults to \'mayhem.config\')')
+    parser.add_argument('--use-pass', action='store_true', help='Use UNIX password store instead of hardcoded tokens')
     parser.add_argument('--log', type=str, default='warn', help='Log level (choose from debug, info, warning, error and critical)')
     parser.add_argument('--insecure', action='store_true', help='Disable SSL verification')
+    parser.add_argument('--dry-run', action='store_true', help='Dry run')
 
 
 
@@ -149,70 +161,66 @@ if __name__ == '__main__':
         requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
     loglevel = args.log.lower() if (args.log.lower() in levels) else 'warn'
     logging.basicConfig(stream=sys.stderr, level=levels[loglevel], format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    namespace = args.namespace
+    workspace = args.workspace
     project = args.project
     target = args.target
-    config = args.config
+    bts_config = args.bts_config
+    mayhem_config = args.mayhem_config
+    use_pass = args.use_pass
+    dry_run = args.dry_run
     if args.bts in BTS.__members__:
         bts = BTS[args.bts]
     else:
         print('You must provide a BTS type with the --bts flag (choices: \'jira\', \'gitlab\')')
         print(parser.print_help())
         sys.exit(1)
-    if args.url:
-        url = args.url
-    elif os.environ.get('MAYHEM_URL'):
-        url = os.environ.get('MAYHEM_URL')
-    else:
-        print('Please either set the url via the --url flag or $MAYHEM_URL variable')
-        print(parser.print_help())
-        sys.exit(1)
-    if args.token:
-        token = args.token
-    elif os.environ.get('MAYHEM_TOKEN'):
-        token = os.environ.get('MAYHEM_TOKEN')
-    else:
-        print('Please either set the API token via the --token flag or $MAYHEM_TOKEN variable')
-        print(parser.print_help())
-        sys.exit(1)
 
-    mayhem_api = url + '/api/v2/namespace/' + namespace + '/project/' + project + '/target/' + target
-    mayhem_headers = {
-        'accept': 'application/json',
-        'X-Mayhem-Token': ('token ' + token)
-    }
-
-    #Ensure API is correct
-    testAPI(url, mayhem_headers)
-
-    with open(config, 'r') as config_file:
+    with open(bts_config, 'r') as config_file:
         config_data = config_file.read()
     bts_api = json.loads(config_data)
+    if use_pass:
+        bts_api[bts.name]['token'] = subprocess.check_output(bts_api[bts.name]['token']).strip().decode('utf-8')
     bts_headers = {
         'Content-Type': 'application/json'
     }
+
+    with open(mayhem_config, 'r') as config_file:
+        config_data = config_file.read()
+    mayhem_api = json.loads(config_data)
+    if use_pass:
+        mayhem_api['mayhem']['token'] = subprocess.check_output(mayhem_api['mayhem']['token']).strip().decode('utf-8')
+    mayhem_headers = {
+        'Content-Type': 'application/json',
+        'X-Mayhem-Token': ('token ' + mayhem_api['mayhem']['token'])
+    }
+
+    #Ensure API is correct
+    testAPI(mayhem_api['mayhem']['url'], mayhem_headers)
+
 
     if bts.name == 'jira':
         ticket = json.loads(JIRA_FORMAT)
         ticket['fields']['project']['key'] = bts_api['jira']['project-key']
         if args.defect:
             defect_id = str(args.defect)
-            defects = getDefect(mayhem_api, mayhem_headers, defect_id)
+            defects = getDefect(mayhem_api, mayhem_headers, workspace, project, target, defect_id)
         elif args.run:
             run_id = str(args.run)
-            defects = getDefectsForRun(mayhem_api, mayhem_headers, run_id)
+            defects = getDefectsForRun(mayhem_api, mayhem_headers, workspace, project, target, run_id)
         else:
             print('Must provide either --defect <id> or --run <id>')
         for defect in defects:
-            ticket['fields']['summary'] = 'Mayhem for Code ' + str(defect['defect_number']) + ': ' + str(defect['title'])
-            ticket['fields']['description'] = str(defect['description']) + '\n' \
-                + 'CWE: ' + str(defect['cwe_number']) + ' ' + str(defect['cwe_description']) + '\n' \
-                + 'Found at: ' + str(defect['defect_hash']) + '\n' \
-                + 'Target: ' + namespace + '/' + project + '/' + target + '\n' \
-                + 'Discovered on: ' + str(defect['created_at']) + '\n'
+            ticket['fields']['summary'] = 'Mayhem ' + str(defect['defect_number']) + ' in ' + project +'/' + target + ': ' + str(defect['title'])
+            ticket['fields']['description'] = str(defect['description']) + '\n\n' \
+                + '*CWE*: ' + str(defect['cwe_number']) + ' ' + str(defect['cwe_description']) + '\n' \
+                + '*Target*: ' + workspace + '/' + project + '/' + target + '\n' \
+                + '*Discovered on*: ' + str(defect['created_at']) + '\n'
+            if defect['examples'][0]['backtrace']:
+                ticket['fields']['description'] += '*Backtrace*: \n```\n' + str(defect['examples'][0]['backtrace']) + '```\n'
             # --todo-- Can set more fields here
-            link = exportToJira(bts_api, bts_headers, ticket)
+            link = exportToJira(bts_api, bts_headers, ticket, dry_run)
             print('Link to newly created JIRA issue: ' + str(link))
+            quit()
     if bts.name == 'gitlab':
         ticket = json.loads(GITLAB_FORMAT)
         bts_headers['PRIVATE-TOKEN'] = bts_api['gitlab']['token']
@@ -225,12 +233,13 @@ if __name__ == '__main__':
         else:
             print('Must provide either --defect <id> or --run <id>')
         for defect in defects:
-            ticket['title'] = 'Mayhem for Code ' + str(defect['defect_number']) + ': ' + str(defect['title'])
+            ticket['title'] = 'Mayhem ' + str(defect['defect_number']) + ' in ' + project +'/' + target + ': ' + str(defect['title'])
             ticket['description'] = str(defect['description']) + '\n\n' \
-                + 'CWE: ' + str(defect['cwe_number']) + ' ' + str(defect['cwe_description']) + '\n\n' \
-                + 'Found at: ' + str(defect['defect_hash']) + '\n\n' \
-                + 'Target: ' + namespace + '/' + project + '/' + target + '\n\n' \
-                + 'Discovered on: ' + str(defect['created_at']) + '\n\n'
+                + '*CWE*: ' + str(defect['cwe_number']) + ' ' + str(defect['cwe_description']) + '\n' \
+                + '*Target*: ' + workspace + '/' + project + '/' + target + '\n' \
+                + '*Discovered on*: ' + str(defect['created_at']) + '\n'
+            if defect['examples'][0]['backtrace']:
+                ticket['fields']['description'] += '*Backtrace*: \n```\n' + str(defect['examples'][0]['backtrace']) + '```\n'
             # --todo-- Can set more fields here
-            link = exportToGitlab(bts_api, bts_headers, ticket)
+            link = exportToGitlab(bts_api, bts_headers, ticket, dry_run)
             print('Link to newly created Gitlab issue: ' + str(link))
