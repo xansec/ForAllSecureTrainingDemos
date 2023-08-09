3250eb71ee1caced1db8e5d0567cc138bc4345ba
Alexander Brewer <abrewer@forallsecure.com>
2023-08-09 10:34:12 -0400

    Change project name

--- module1/petstore-api-demo/runmapi.sh
+++ module1/petstore-api-demo/runmapi.sh
@@ -3,7 +3,7 @@
 set -ex
 
 mapi run \
-     forallsecure/petstore/petstore 30 'http://localhost/openapi.json' \
+     training/petstore/petstore 30 'http://localhost/openapi.json' \
      --header-auth 'api_key: special-key' \
      --sarif 'output.sarif' \
      --url 'http://localhost/v3/' \
