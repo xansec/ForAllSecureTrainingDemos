e0d268c78b2a0b931ae8140348c706b798ecafad
xansec <abrewer@forallsecure.com>
2023-10-17 10:45:41 -0400

    remove sarif

--- module1/petstore-api-demo/runmapi.sh
+++ module1/petstore-api-demo/runmapi.sh
@@ -5,7 +5,6 @@ set -ex
 mapi run \
      training/petstore/petstore 30 'http://localhost/openapi.json' \
      --header-auth 'api_key: special-key' \
-     --sarif 'output.sarif' \
      --url 'http://localhost/v3/' \
      --interactive \
      "${@}" 
