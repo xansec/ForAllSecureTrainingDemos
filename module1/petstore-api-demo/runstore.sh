b700b933c075ffef8500027bf4ce020d94a99e0c
xansec <abrewer@forallsecure.com>
2024-10-03 10:26:06 -0400

    remove container

--- module1/petstore-api-demo/runstore.sh
+++ module1/petstore-api-demo/runstore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #docker pull openapitools/openapi-petstore
-docker run -it -e OPENAPI_BASE_PATH=/v3 -p 80:8080 openapitools/openapi-petstore
+docker run --rm -it -e OPENAPI_BASE_PATH=/v3 -p 80:8080 openapitools/openapi-petstore
